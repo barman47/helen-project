d03763ce994e0bf0cd4bee86455d85ad68611b62
barman47 <nomsouzoanya@yahoo.co.uk>
2018-10-05 16:33:11 +0100

    Final Deploy

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // database: 'mongodb://localhost:27017/helenchatapp',
-    database: 'mongodb://barman:VICEcity47@ds123783.mlab.com:23783/helenchatapp',
+    database: 'mongodb://localhost:27017/helenchatapp',
+    // database: 'mongodb://barman:VICEcity47@ds123783.mlab.com:23783/helenchatapp',
     secret: 'barman secret'
 };
